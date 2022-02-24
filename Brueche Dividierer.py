457156a40d0526f96c98fd3cc93f9defa1987618
Liande Guo <47792949+LyrikTasche@users.noreply.github.com>
2022-02-24 21:28:02 +0100

    First Version

---
+++ Brueche Dividierer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+#Coded by @LyrikTasche - Strings and Variable names in german
+zähler1 = int(input("Enter Counter 1:"))
+zähler2 = int(input("Enter Counter 2:"))
+nenner1 = int(input("Enter denominator 1:"))
+nenner2 = int(input("Enter denominator 2:"))
+
+#result1 =  zähler1 * nenner2
+#result2 = zähler2 * nenner1
+
+#print(result1 + "/" + result2)
+output1 = zähler1*nenner2
+output2 = zähler2*nenner1
+
+print(output1)
+print("/")
+print(output2)
+
+		
+	
+		
